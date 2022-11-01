e3d6746b7ba00ad5d39cebe103b1e50192840d36
John Ackley <john@johnackley.com>
2022-11-01 07:25:23 -0400

    seems to work well enough - now for some coffee

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,57 @@
+import { exec } from 'child_process';
 import { Wordsy } from './wordsy';
 
-function main(): void {
-  const wordsy = new Wordsy();
-  
-  console.log(wordsy.shell());
+async function main({dryrun = false}): Promise<void> {
+  const wordsy = new Wordsy(6)
+    .addAnchor(1, 'r')
+    .addAnchor(2, 'o')
+    .addFloater('t')
+    .addFloater('r')
+    .addDud('u')
+    .addDud('n')
+    .addDud('f')
+    .addDud('l')
+    .addDud('d')
+    .addDud('s')
+    .addDud('k')
+    .addDud('e')
+    .addDud('p')
+    .addDud('i')
+    .addDud('c')
+    .addBad(1, 't')
+    .addBad(0, 't')
+    .addBad(5, 'r')
+    ;
+  const cmd = wordsy.shell();
+  if (dryrun) {
+    console.log(cmd);
+    return;
+  }
+  await exec(cmd, (error, stdout, stderr) => {
+    console.log('stdout:', stdout);
+    if (error !== null) {
+      console.log('exec error: ', error);
+    }
+  });
 }
 
-main();
+main({dryrun: true});
+
+// .addAnchor(1, 'r')
+// .addAnchor(2, 'o')
+//   .addFloater('t')
+//   .addFloater('r')
+//   .addDud('u')
+//   .addDud('n')
+//   .addDud('f')
+//   .addDud('l')
+//   .addDud('d')
+//   .addDud('s')
+//   .addDud('k')
+//   .addDud('e')
+//   .addDud('p')
+//   .addDud('i')
+//   .addDud('c')
+//   .addBad(1, 't')
+//   .addBad(0, 't')
+//   .addBad(5, 'r')
--- src/wordsy.ts
+++ src/wordsy.ts
@@ -1,42 +1,121 @@
+
 export class Wordsy {
   len : number;
-  firstAnchor : string | undefined = undefined;
   anchors : string[] = [];
   duds : string[] = [];
-  badAnchors : string[] = [];
+  bads : string[] = [];
   floaters : string[] = [];
 
   constructor (len = 5) {
     this.len = len;
+    for (let n = 0; n < this.len; n++) {
+      this.anchors[n] = '.';
+    }
+  }
+
+  public firstIsWild() : boolean {
+    return this.anchors[0] === '.';
+  }
+
+  public assertSingleAlpha(chr : string, msg : string) : void {
+    if (!chr || chr.length != 1 || !/^[a-z]$/i.test(chr)) {
+      throw new Error(`${msg} may only be a single alpha char`);
+    }
+  }
+
+  public assertPosBounds(pos: number, msg : string) : void {
+    if (pos < 0 || pos >= this.len) {
+      throw new Error(`${msg} pos must be between 0 and len`);
+    }
+  }
+
+  public assertExactForm(str : string, msg : string) : void {
+    if (!str) {
+      throw new Error(`${msg} must be a string containing alphas or '.' exactly ${this.len} long`);
+    }
+    if (str.length !== this.len) {
+      throw new Error(`${msg} len must be exactly ${this.len}`);
+    }
+    if (!str.match(/[a-z.]+/)) {
+      throw new Error(`${msg} must be a string containing alphas or '.' exactly ${this.len} long`);
+    }
+  }
+
+  public assertAlphas(str : string, msg : string) : void {
+    if (!str || str.length > 0 || !/^[a-z]+$/i.test(str)) {
+      throw new Error(`${msg} may only be string of alpha chars`);
+    }
+  }
+
+  public setAnchors(str : string) : Wordsy  {
+    this.assertExactForm(str, 'setAnchors');
+    for (let i = 0; i < this.len; i++) {
+      this.anchors[i] = str.charAt(i);
+    }
+    return this;
+  }
+
+  public addAnchor(pos : number, chr : string) : Wordsy {
+    this.assertSingleAlpha(chr, 'anchor');
+    this.assertPosBounds(pos, 'anchor');
+    this.anchors[pos] = chr;
+    return this;
+  }
+
+  public addBad(pos : number, chr : string) : Wordsy {
+    this.assertSingleAlpha(chr, 'bad');
+    this.assertPosBounds(pos, 'bad');
+    this.bads.push('^' + '.'.repeat(pos) + chr + '.'.repeat(this.len - 1 - pos) + '$');
+    return this;
+  }
+
+  public setFloaters(str : string) : Wordsy {
+    this.assertAlphas(str, 'setFloaters');
+    this.floaters = str.split('');
+    return this;
+  }
+
+  public addFloater(chr : string) : Wordsy {
+    this.assertSingleAlpha(chr, 'floater');
+    this.floaters.push(chr);
+    return this;
+  }
+
+  public addDud(chr : string) : Wordsy {
+    this.assertSingleAlpha(chr, 'dud');
+    this.duds.push(chr);
+    return this;
   }
 
   public shell() {
     const pipes : string[] = [];
 
     // firstAnchors
-    pipes.push(this.firstAnchor
-      ? `look ${this.firstAnchor}`
-      : 'for f in `a-z`; do look $f');
+    pipes.push(this.firstIsWild()
+      ? 'for f in `a-z`; do look $f'
+      : `look ${this.anchors[0]}`
+    );
 
     // anchors
-    pipes.push(!this.anchors.length
-      ? `egrep '^.....$'`
-      : `egrep '${this.anchors.join('|')}`);
+    pipes.push(`egrep '^${this.anchors.join('')}$'`);
 
     // duds
-    const duds = `egrep -v "[A-Z${this.duds.join()}]`
-
-    // bad anchors
-    const badanchors = this.badAnchors.length
-        ? `egrep -v '${this.badAnchors.join('|')}'`
-        : '';
-
-    // floaters
-    if (this.floaters.length) {
-      pipes.push(`egrep -v '${this.floaters.join('|')}'`);
+    if (this.duds.length) {
+      pipes.push(`egrep -v '[A-Z${this.duds.join('')}]'`);
     }
 
+    // bad anchors
+    if (this.bads.length) {
+      pipes.push(`egrep -v '${this.bads.join('|')}'`);
+    }
+
+    // floaters
+    this.floaters.forEach(f => {
+      pipes.push(`egrep '${f}'`);
+    });
+
     // done
-    return pipes.join(' | ') + (!this.firstAnchor ? '; done' : '');
+    return pipes.join(' | ') + (this.firstIsWild() ? '; done' : '');
   }
+
 }
