8987f42479cc1dc35e42a8ffc7fa8b85d73b8ac7
John Ackley <john@johnackley.com>
2023-01-02 08:48:41 -0500

    move brute here and add addmul3

---
+++ src/brute.test.ts
@@ -0,0 +1,22 @@
+import { brute } from './brute';
+
+// 12+46=58
+test('initial guess', () => {
+  const expected = '12+46=58';
+  const actual = brute([1, 2, 4, 5, 6, 8]);
+  expect(actual.includes(expected)).toBeTruthy();
+});
+
+// 49*3=147
+test('mul3 guess', () => {
+  const expected = '49*3=147';
+  const actual = brute([1, 3, 4, 7, 9]);
+  expect(actual.includes(expected)).toBeTruthy();
+});
+
+// 9+3*2=15
+test('summul guess', () => {
+  const expected = '9+3*2=15';
+  const actual = brute([1, 2, 3, 5, 9]);
+  expect(actual.includes(expected)).toBeTruthy();
+});
---
+++ src/brute.ts
@@ -0,0 +1,98 @@
+type OpType = {
+  num: number;
+  fun: (nums : number[]) => OpResult;
+}
+
+type OpResult = {
+  res: number;
+  str: string;
+}
+
+const ops = [
+  { num: 3, fun: mul3 },
+  { num: 3, fun: add3 },
+  { num: 3, fun: addmul3 },
+  { num: 4, fun: add4 },
+]
+
+export function brute(nums : number[]) : string[] {
+  const ret : string[] = [];
+  const numsSet = new Set(nums);
+  const len = nums.length;
+  for (let o = 0; o < ops.length; o++) {
+    const op = ops[o]
+    for (let h = 0; h < len; h++) {
+      for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+          if (op.num === 3) {
+            const opands = [nums[h], nums[i], nums[j]];
+            const opres = op.fun(opands);
+            if (hasAllAndOnly(numsSet, opands, opres)) {
+              ret.push(opres.str);
+            }
+          }
+          for (let k = 0; k < len; k++) {
+            if (op.num === 4) {
+              const opands = [nums[h], nums[i], nums[j], nums[k]];
+              const opres = op.fun(opands);
+              if (hasAllAndOnly(numsSet, opands, opres)) {
+                ret.push(opres.str);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return ret;
+}
+
+function hasAllAndOnly(reqSet : Set<number>, opands : number[], opres : OpResult) : boolean {
+  if (opres.str.length !== 8) {
+    return false;
+  }
+  const solSet = new Set([...opands, ...`${opres.res}`.split('').map(n => parseInt(n))]);
+  if (reqSet.size === solSet.size &&
+    [...reqSet].every((x) => solSet.has(x))) {
+    return true;
+  }
+  return false;
+}
+
+function mul3(nums : number[]) : OpResult {
+  const x = nums[0];
+  const y = nums[1];
+  const z = nums[2];
+  const res = ((x * 10) + y) * z;
+  return {res, str: `${x}${y}*${z}=${res}`};
+}
+
+function add3(nums : number[]) : OpResult {
+  const x = nums[0];
+  const y = nums[1];
+  const z = nums[2];
+  const res = ((x * 10) + y) + z;
+  return {res, str: `${x}${y}+${z}=${res}`};
+}
+
+function add4(nums : number[]) : OpResult {
+  const w = nums[0];
+  const x = nums[1];
+  const y = nums[2];
+  const z = nums[3];
+  const res = ((w * 10) + x) + ((y * 10) + z);
+  return {res, str: `${w}${x}+${y}${z}=${res}`};
+}
+
+function addmul3(nums : number[]) : OpResult {
+  const x = nums[0];
+  const y = nums[1];
+  const z = nums[2];
+  const res = x + (y * z);
+  return {res, str: `${x}+${y}*${z}=${res}`};
+}
+
+const nums : number[] = process.argv.slice(2).map(n => parseInt(n));
+console.log(`bruting: ${nums}`);
+const res = brute(nums);
+console.log(res.join('\n'));
