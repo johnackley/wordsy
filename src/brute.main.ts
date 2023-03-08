2924f46b36643aa3ebda78a3f7c672705fbe9aa2
John Ackley <john@johnackley.com>
2023-03-08 06:30:29 -0500

    nit usage

--- src/brute.main.ts
+++ src/brute.main.ts
@@ -4,11 +4,11 @@ export function main() {
   const equalsPos = parseInt(process.argv[2]);
   const allowedOps = process.argv[3];
   if (!allowedOps.match(/[/*+-]+/)) {
-    console.log('usage: run brute [ops] dig [dig...]');
+    console.log('usage: run brute equalpos allowedops dig [dig...]');
     process.exit();
   }
   const nums : number[] = process.argv.slice(4).map(n => parseInt(n));
-  console.log(`bruting (= @ ${equalsPos}) '${allowedOps}': ${nums}`);
+  console.log(`bruting [${equalsPos}:${allowedOps}]: ${nums}`);
   const res = brute(equalsPos, allowedOps, nums);
   console.log(res.join('\n'));
 }
